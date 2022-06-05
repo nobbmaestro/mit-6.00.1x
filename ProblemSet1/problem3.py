e6fd8bae6df504e05093989bc925d5bc41a0fb4c
Norbert Batiuk <norbert@batiuk.se>
2022-06-05 22:02:05 +0200

    fixing type errors

--- ProblemSet1/problem3.py
+++ ProblemSet1/problem3.py
@@ -4,10 +4,10 @@
 
 # Write a program that prints the longest substring of s in which the letters occur in alphabetical order. 
 # For example, if s = 'azcbobobegghakl', then your program should print
-# output: Longest substring in alphabetical order is: beggh
+# > Longest substring in alphabetical order is: beggh
 
 # In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print
-# output: Longest substring in alphabetical order is: abc
+# > Longest substring in alphabetical order is: abc
 # Note: This problem may be challenging. We encourage you to work smart. If you've spent more than a few hours on this problem, 
 # we suggest that you move on to a different part of the course. If you have time, come back to this problem after you've had a break and cleared your head.
 
