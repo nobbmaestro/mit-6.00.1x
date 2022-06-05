58b3642e132b828c50b2738abb831d5368fffe37
Norbert Batiuk <norbert@batiuk.se>
2022-06-05 23:11:57 +0200

    Final version of Unit 2 added

--- ProblemSet2/test_problem1.py
+++ ProblemSet2/test_problem1.py
@@ -17,11 +17,11 @@ class TestProblem1(unittest.TestCase):
     def test_output(self):
         msg = 'Output at index {i} does not match expected output of \'{output}\''
         for i in self.test_data.keys():
-            balance = self.test_data[i]['input'][0]
-            annualInterestRate = self.test_data[i]['input'][1]
-            monthlyPaymentRate = self.test_data[i]['input'][2]
+            arg1 = self.test_data[i]['input'][0]
+            arg2 = self.test_data[i]['input'][1]
+            arg3 = self.test_data[i]['input'][2]
 
-            result = func(balance, annualInterestRate, monthlyPaymentRate)
+            result = func(arg1, arg2, arg3)
             expected_output = self.test_data[i]['output']
 
             self.assertEqual(result, expected_output, msg.format(i=i, output=expected_output))
--- ProblemSet2/test_problem2.py
+++ ProblemSet2/test_problem2.py
@@ -25,10 +25,10 @@ class TestProblem1(unittest.TestCase):
     def test_output(self):
         msg = 'Output at index {i} does not match expected output of \'{output}\''
         for i in self.test_data.keys():
-            balance = self.test_data[i]['input'][0]
-            annualInterestRate = self.test_data[i]['input'][1]
+            arg1 = self.test_data[i]['input'][0]
+            arg2 = self.test_data[i]['input'][1]
 
-            result = func(balance, annualInterestRate)
+            result = func(arg1, arg2)
             expected_output = self.test_data[i]['output']
 
             self.assertEqual(result, expected_output, msg.format(i=i, output=expected_output))
