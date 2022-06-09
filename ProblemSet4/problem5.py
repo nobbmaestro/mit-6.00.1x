6084b38808e4e7cc704446114489efeed6b852eb
Norbert Batiuk <norbert@batiuk.se>
2022-06-09 23:02:15 +0200

    small error fixes for Unit4

--- ProblemSet4/problem5.py
+++ ProblemSet4/problem5.py
@@ -3,9 +3,9 @@
 # In ps4a.py, note that in the function playHand, there is a bunch of pseudocode. This pseudocode is provided to help
 # guide you in writing your function. Check out the Why Pseudocode? resource to learn more about the What and Why of
 # Pseudocode before you start coding your solution.
-
+#
 # Note: Do not assume that there will always be 7 letters in a hand! The parameter n represents the size of the hand.
-
+#
 # Testing: Before testing your code in the answer box, try out your implementation as if you were playing the game. 
 # Here is some example output of playHand:
 
--- ProblemSet4/problem6.py
+++ ProblemSet4/problem6.py
@@ -4,7 +4,7 @@
 # Write the code that implements the playGame function. You should remove the code that is currently uncommented in the 
 # playGame body. Read through the specification and make sure you understand what this function accomplishes. For the 
 # game, you should use the HAND_SIZE constant to determine the number of cards in a hand.
-
+#
 # Testing: Try out this implementation as if you were playing the game. Try out different values for HAND_SIZE with 
 # your program, and be sure that you can play the wordgame with different hand sizes by modifying only the variable 
 # HAND_SIZE.
